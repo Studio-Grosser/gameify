4c5372f245197338f87995be736890a182dbf667
linusgrosser <linus.grosser.lg@gmail.com>
2024-10-12 14:23:29 +0200

    add logging to database services

--- lib/database/date_service.dart
+++ lib/database/date_service.dart
@@ -1,71 +1,99 @@
-import 'dart:developer';
-
 import 'package:gameify/database/database_service.dart';
 import 'package:gameify/models/date.dart';
+import 'package:gameify/utils/logger.dart';
 import 'package:gameify/utils/utils.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DateService {
   Future<Date?> readDate(String id) async {
     final db = await DatabaseService.instance.database;
-    final dates = await db.query(
-      Date.tableName,
-      columns: [Date.id_, Date.completedTaskIds_],
-      where: '${Date.id_} = ?',
-      whereArgs: [id],
-    );
 
-    if (dates.isEmpty) return null;
-    return Date.fromJson(dates.first);
+    try {
+      final dates = await db.query(
+        Date.tableName,
+        columns: [Date.id_, Date.completedTaskIds_],
+        where: '${Date.id_} = ?',
+        whereArgs: [id],
+      );
+      Logger.s('Queried Date "$id" from database');
+      if (dates.isEmpty) return null;
+      return Date.fromJson(dates.first);
+    } catch (e, st) {
+      Logger.e('Error querying date "$id": ${e.toString()}', st);
+      return null;
+    }
   }
 
   Future<void> writeDate(Date date) async {
     final db = await DatabaseService.instance.database;
-    await db.insert(Date.tableName, date.toJson(),
-        conflictAlgorithm: ConflictAlgorithm.replace);
-    log('date added');
+
+    try {
+      await db.insert(Date.tableName, date.toJson(),
+          conflictAlgorithm: ConflictAlgorithm.replace);
+      Logger.s('Date "${fromId(date.id)}" added to database');
+    } catch (e, st) {
+      Logger.e('Error inserting date: ${e.toString()}', st);
+    }
   }
 
   Future<int> getHighestScore() async {
     final db = await DatabaseService.instance.database;
-    final List<Map<String, dynamic>> result = await db
-        .rawQuery('SELECT MAX(score) as max_score FROM ${Date.tableName}');
 
-    if (result.isNotEmpty && result.first['max_score'] != null) {
-      return result.first['max_score'] as int;
-    } else {
-      return 0;
+    try {
+      final List<Map<String, dynamic>> result = await db
+          .rawQuery('SELECT MAX(score) as max_score FROM ${Date.tableName}');
+      Logger.s('Queried highest score from database');
+      if (result.isNotEmpty && result.first['max_score'] != null) {
+        int maxScore = result.first['max_score'] as int;
+        Logger.i('Highest score is $maxScore');
+        return maxScore;
+      }
+    } catch (e, st) {
+      Logger.e('Error getting highest score: ${e.toString()}', st);
     }
+    return 0;
   }
 
   Future<double> getAverageScore() async {
     final db = await DatabaseService.instance.database;
-    final List<Map<String, dynamic>> result = await db
-        .rawQuery('SELECT AVG(score) as avg_score FROM ${Date.tableName}');
 
-    if (result.isNotEmpty && result.first['avg_score'] != null) {
-      return result.first['avg_score'] as double;
-    } else {
-      return 0;
+    try {
+      final List<Map<String, dynamic>> result = await db
+          .rawQuery('SELECT AVG(score) as avg_score FROM ${Date.tableName}');
+
+      if (result.isNotEmpty && result.first['avg_score'] != null) {
+        double averageScore = result.first['avg_score'] as double;
+        Logger.i('Average score is $averageScore');
+        return averageScore;
+      }
+    } catch (e, st) {
+      Logger.e('Error getting average score: ${e.toString()}', st);
     }
+    return 0;
   }
 
   Future<Map<DateTime, double>> getHeatMapData(int highscore) async {
     final db = await DatabaseService.instance.database;
-    final List<Map<String, dynamic>> results = await db.query(Date.tableName);
 
-    Map<DateTime, double> scoreMap = {};
+    try {
+      final List<Map<String, dynamic>> results = await db.query(Date.tableName);
 
-    for (var row in results) {
-      String id = row[Date.id_];
-      int score = row[Date.score_] ?? 0;
+      Map<DateTime, double> scoreMap = {};
 
-      DateTime dateTime = fromId(id);
-      double relativeScore = highscore > 0 ? score / highscore : 0.0;
+      for (var row in results) {
+        String id = row[Date.id_];
+        int score = row[Date.score_] ?? 0;
 
-      scoreMap[dateTime] = relativeScore;
+        DateTime dateTime = fromId(id);
+        double relativeScore = highscore > 0 ? score / highscore : 0.0;
+
+        scoreMap[dateTime] = relativeScore;
+      }
+      Logger.s('Queried heat map data from database');
+      return scoreMap;
+    } catch (e, st) {
+      Logger.e('Error getting average score: ${e.toString()}', st);
+      return {};
     }
-
-    return scoreMap;
   }
 }
